5bdf01e213bb131fbcf79aab7d7040a974536f4e
Clwmm <emgie4432@gmail.com>
2024-10-29 16:01:59 +0100

    OAuth with static frontend

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,38 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
 from backend.routers import users
-
+from backend.auth import auth
 app = FastAPI()
-origins = ["http://localhost"]
+app.add_middleware(SessionMiddleware, secret_key="add any string...")
+app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app.include_router(users.router, tags=["users"])
+app.include_router(auth.router, tags=["auth"])
 
-app.include_router(users.router, prefix="/users", tags=["users"])
+templates = Jinja2Templates(directory="backend/templates")
 
-@app.get('/')
-async def root():
-    return {"message": "Fast Api check!"}
+@app.get("/")
+def index(request: Request):
+    user = request.session.get('user')
+    if user:
+        return RedirectResponse('welcome')
+
+    return templates.TemplateResponse(
+        name="home.html",
+        context={"request": request}
+    )
+
+@app.get('/welcome')
+def welcome(request: Request):
+    user = request.session.get('user')
+    if not user:
+        return RedirectResponse('/')
+    return templates.TemplateResponse(
+        name='welcome.html',
+        context={'request': request, 'user': user}
+    )
